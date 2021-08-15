5d0d09c491a275af774908891f58da7aab8d8c52
tonyjahim <tonyjahim@gmail.com>
2021-08-15 20:49:24 +0200

    add react-component-type template

---
+++ react-component-type/MyComponent.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import { TMyComponentComponent } from './MyComponent.types'
+
+const MyComponent: TMyComponentComponent = () => {
+
+    return (
+        <div className="MyComponent">
+
+        </div>
+    )
+}
+
+export default MyComponent
